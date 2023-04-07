4dc91b7bbf0ac2f8de40d767b9ebaa95404b18dd
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-07 18:26:32 +0900

    #2. Admin Customizing

--- houses/admin.py
+++ houses/admin.py
@@ -4,6 +4,12 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
+    fields = (
+        "name",
+        "address",
+        ("price_per_night", "pets_allowed"),
+    )
+
     list_display = (
         "name",
         "price_per_night",
@@ -18,3 +24,4 @@ class HouseAdmin(admin.ModelAdmin):
 
     search_fields = ("address",)
     search_help_text = "Search Location"
+    list_editable = ("pets_allowed",)
