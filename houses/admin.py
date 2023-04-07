6584c2bf003cc5cd3e732d2acb7baa2fae585ed1
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-07 18:21:01 +0900

    #2. Houses Admin

--- houses/admin.py
+++ houses/admin.py
@@ -4,4 +4,17 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed",
+    )
+
+    list_filter = (
+        "price_per_night",
+        "pets_allowed",
+    )
+
+    search_fields = ("address",)
+    search_help_text = "Search Location"
