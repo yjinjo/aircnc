a7a93f6d8f3bfc96658c2c8919653632e99ccfd7
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-07 18:09:02 +0900

    #2. House 모델 field 이름 변경

--- houses/models.py
+++ houses/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class House(models.Model):
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
