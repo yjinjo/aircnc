f7276c527bef3809b7fbfec781e56a74e41067a7
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-10 18:27:39 +0900

    #7. Serializer Context

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -21,6 +21,7 @@ class RoomDetailSerializer(ModelSerializer):
     category = CategorySerializer(read_only=True)
 
     rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -29,9 +30,14 @@ class RoomDetailSerializer(ModelSerializer):
     def get_rating(self, room):
         return room.rating()
 
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
+
 
 class RoomListSerializer(ModelSerializer):
     rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -42,7 +48,12 @@ class RoomListSerializer(ModelSerializer):
             "city",
             "price",
             "rating",
+            "is_owner",
         )
 
     def get_rating(self, room):
         return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
