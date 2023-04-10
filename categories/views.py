1921bfbdc97f1ab70baaf659ae02b8ba1aefd0e9
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-10 15:52:43 +0900

    #6. update()

--- categories/views.py
+++ categories/views.py
@@ -2,6 +2,7 @@ from django.core import serializers
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 
 from categories.models import Category
@@ -26,8 +27,24 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(("GET", "PUT"))
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
