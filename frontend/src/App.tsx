821b8543f844e62497a3361213a4988896597c6c
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-11 11:22:24 +0900

    #12. Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text } from "@chakra-ui/react";
+
+export const App = () => (
+  <div>
+    <Text color={"red.600"} fontSize={"6xl"}>
+      It works!!!
+    </Text>
+  </div>
+);
---
+++ frontend/src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
