ab1e65e45dfb6bb3ebee5992d12bfe8593c9b0b3
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-11 14:11:09 +0900

    #14. Devtools and Query Keys

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { QueryFunctionContext } from "@tanstack/react-query";
 
 const instance = axios.create({
   baseURL: "http://localhost:8000/api/v1",
@@ -7,5 +8,7 @@ const instance = axios.create({
 export const getRooms = () =>
   instance.get("rooms/").then((response) => response.data);
 
-export const getRoom = () =>
-  instance.get(`rooms/2`).then((response) => response.data);
+export const getRoom = ({ queryKey }: QueryFunctionContext) => {
+  const [_, roomPk] = queryKey;
+  return instance.get(`rooms/${roomPk}`).then((response) => response.data);
+};
