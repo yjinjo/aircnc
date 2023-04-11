7ca9e757334c85f60a9055b4c7ed164de498c3ec
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-11 10:30:02 +0900

    #11. Create Amenity Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -7,6 +7,7 @@ from rooms import models
 class TestAmenities(APITestCase):
     NAME = "Amenity Test"
     DESC = "Amenity Desc"
+    URL = "/api/v1/rooms/amenities/"
 
     def setUp(self):
         models.Amenity.objects.create(
@@ -39,3 +40,30 @@ class TestAmenities(APITestCase):
             data[0]["description"],
             self.DESC,
         )
+
+    def test_create_amenity(self):
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Amenity desc."
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, 200, "Not 200 status code.")
+        self.assertEqual(
+            data["name"],
+            new_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            new_amenity_description,
+        )
+
+        response = self.client.post(self.URL)
+        data = response.json()
+        self.assertNotEqual(response.status_code, 400)
+        self.assertIn("name", data)
