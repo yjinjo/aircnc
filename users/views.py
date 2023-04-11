3123511836affb4204ce43f0867377dd017184a0
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-11 09:44:16 +0900

    #8. Change Password

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from rest_framework.exceptions import ParseError
+from rest_framework import status
+from rest_framework.exceptions import ParseError, NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users import serializers
+from users.models import User
 
 
 class Me(APIView):
@@ -44,3 +46,32 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise NotFound
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        current_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+
+        if not current_password or not new_password:
+            raise ParseError
+
+        if user.check_password(current_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
